44da870a630d700d0228a831c7def485ac3ea090
Pranjal Chaurasiya <92513144+Its-Pranjal@users.noreply.github.com>
2023-04-30 00:07:53 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Form from './components/form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-center'>Let's Generate your resume</h1>
+      <Form /> {/* render the form */}
+    </div>
+  );
+}
+
+export default App;
